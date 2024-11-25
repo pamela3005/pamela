9bbbf5fb3bb13e7b5b5101b069aa23f90a692859
pamela3005 <pamelagaby3005@gmail.com>
2024-11-25 12:56:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    ' Qual é o menor país do mundo em extensão territorial? ',
+    'Vaticano'
+)
+
+criaCartao(
+    'Geral',
+    'Qual elemento químico tem o símbolo O na tabela periódica?',
+    'Oxigênio'
+)
+
+criaCartao(
+    'Geral',
+    'Em que ano ocorreu a Primeira Guerra Mundial?',
+    '1914'
+)
+
+criaCartao(
+    'Geral',
+    'Qual planeta do sistema solar é conhecido como o "Planeta Vermelho"?',
+    'Marte '
+)
